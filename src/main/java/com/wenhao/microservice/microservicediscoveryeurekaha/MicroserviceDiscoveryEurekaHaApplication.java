e9f1c2d09ccd77cdc0eed9a7eed1f7a2aff36b00
wenhao176 <921790476@qq.com>
2018-12-05 14:30:27 +0800

    eureka高可用

---
+++ src/main/java/com/wenhao/microservice/microservicediscoveryeurekaha/MicroserviceDiscoveryEurekaHaApplication.java
@@ -0,0 +1,12 @@
+package com.wenhao.microservice.microservicediscoveryeurekaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDiscoveryEurekaHaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDiscoveryEurekaHaApplication.class, args);
+    }
+}
